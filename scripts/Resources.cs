9d4b9b27229b7ca381f2d3e0bc3461ab96160f94
40180968 <msullivan07@qub.ac.uk>
2021-06-13 01:40:07 +0100

    Extend Resources enum with texture mapping

--- scripts/Resources.cs
+++ scripts/Resources.cs
@@ -13,3 +13,36 @@ public enum Resources
         Steel,
         Wood
     }
+
+public static class ResourcesMethods
+{
+    public static String GetResourceNameForResource(Resources resource)
+    {
+        switch (resource)
+        {
+            case Resources.Alcohol:
+                return "Carriage Oli and Alcohol.PNG";
+            case Resources.Apples:
+                return "Apple and Mail Carriage.PNG";
+            case Resources.Coal:
+                return "Coal Carriage.PNG";
+            case Resources.Dynamite:
+                return "Carriage Gold Gem Dynamite.PNG";
+            case Resources.Gems:
+                return "Carriage Gold Gem Dynamite.PNG";
+            case Resources.Gold:
+                return "Carriage Gold Gem Dynamite.PNG";
+            case Resources.Mail:
+                return "Apple and Mail Carriage.PNG";
+            case Resources.Oil:
+                return "Carriage Oli and Alcohol.PNG";
+            case Resources.Steel:
+                return "Steel Carriage.PNG";
+            case Resources.Wood:
+                return "Steel Carriage.PNG";
+            default:
+                return null;
+        }
+    }
+
+}
